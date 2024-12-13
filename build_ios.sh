0493fbc1d81ab3d218a1049eaf1a1c299693c696
Cesar Guirao <cesar.guirao@riverside.fm>
2024-12-12 19:31:37 -0800

    Changes to compile mac for WebRTC local development

--- build_ios.sh
+++ build_ios.sh
@@ -161,6 +161,7 @@ function patchWebRTC() {
 	patch -b -p0 -d $WORK_DIR < $PATCHES_DIR/video_encoder_factory_h.patch
     patch -b -p0 -d $WORK_DIR < $PATCHES_DIR/objc_audio_device_module_h.patch
     patch -b -p0 -d $WORK_DIR < $PATCHES_DIR/objc_audio_device_module_mm.patch
+	patch -b -p0 -d $WORK_DIR < $PATCHES_DIR/absl_threadlocal.patch
 }
 
 function refetchWebRTC() {

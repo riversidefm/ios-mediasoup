7ec64c45966b4a2236d6d3442194cf18aea6790d
Shai Mishali <freak4pc@gmail.com>
2026-03-17 18:50:09 +0200

    Release 1.0.21

--- Package.swift
+++ Package.swift
@@ -23,12 +23,12 @@ let package = Package(
         ),
         .binaryTarget(
             name: "Mediasoup",
-            url: "https://github.com/riversidefm/ios-mediasoup/releases/download/1.0.20/Mediasoup.xcframework.zip",
+            url: "https://github.com/riversidefm/ios-mediasoup/releases/download/1.0.21/Mediasoup.xcframework.zip",
             checksum: "a1dbe69aaf95027b9e93cf436f03c4eeb5902c04aa6edbb0b72335fdba8c5685"
         ),
         .binaryTarget(
             name: "WebRTC",
-            url: "https://github.com/riversidefm/ios-mediasoup/releases/download/1.0.20/WebRTC.xcframework.zip",
+            url: "https://github.com/riversidefm/ios-mediasoup/releases/download/1.0.21/WebRTC.xcframework.zip",
             checksum: "d4d2b434dae5b43d3aea0bd02788ebdf88d6ea58532ca18b46ca2c8b4ba505cf"
         )
     ]
